6a39ae37317c5da8278b0fb2196aa6ce8c62f2f6
Decipher3859 <accounts@damianschumachers.com>
2025-04-29 20:26:21 +0200

    Footer angepasst, Teaser angepasst.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh damian@midnight-worker.de "cd /var/www/html/B7-Website_Vanilla && git pull"
+ssh webuser@midnight-worker.de "cd /var/www/html/B7-Website_Vanilla && git pull origin main && git pull origin develop"
