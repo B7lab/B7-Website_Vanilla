784b31a316c4369a5501e6e71ada52bd164cbf5f
Decipher3859 <accounts@damianschumachers.com>
2025-04-30 13:46:32 +0200

    Footer eingefügt.

--- views/template/layout.php
+++ views/template/layout.php
@@ -42,7 +42,59 @@
     </header>
     
     <?php include $viewFile; ?>
+    <!-- <div class="color-band" id="footer-color-band" style="background-color: var(--technikblau);"></div>
+        <footer id="main-footer">
+            <div class="container">
+                <div id="footer-links">
+                    <ul>
+                        <li><a href="kontakt.php">Kontakt</a></li>
+                        <li><a href="datenschutz.php">Datenschutz</a></li>
+                        <li><a href="impressum.php">Impressum</a></li>
+                        <li>
+                            <a href="https://facebook.com" target="_blank">
+                                <img src="public/img/icon/facebook-icon_800x800.svg" alt="Facebook">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://instagram.com" target="_blank">
+                                <img src="public/img/icon/instagram-icon_800x800.svg" alt="Instagram">
+                        </a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="footer-copyright">
+                    <p>&copy; 2025 Blumenthal 7 e.V. Alle Rechte vorbehalten.</p>
+                </div>
+            </div>
+        </footer>
+    </div> -->
 
+    <footer id="main-footer">
+    <div class="color-band" id="footer-color-band" style="background-color: var(--technikblau);">
+        <div class="container">
+            <div id="footer-links">
+                <ul>
+                    <li><a href="kontakt.php">Kontakt</a></li>
+                    <li><a href="datenschutz.php">Datenschutz</a></li>
+                    <li><a href="impressum.php">Impressum</a></li>
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <img src="public/img/icon/facebook-icon_800x800.svg" alt="Facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <img src="public/img/icon/instagram-icon_800x800.svg" alt="Instagram">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>    
+    <div id="footer-copyright">
+                <p>&copy; 2025 Blumenthal 7 e.V. Alle Rechte vorbehalten.</p>
+            </div>
+</footer>
     <script>
     function toggleMenu() {
         const menu = document.getElementById('mainMenu');
