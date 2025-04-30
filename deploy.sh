d92349c699c58ab02d364981d5cf3ef280cd959d
Maik @ Spaceship <maik.tappe82@googlemail.com>
2025-04-30 02:23:30 +0200

    deploy.sh korrigiert

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh damian@midnight-worker.de "cd /var/www/html/B7-Website_Vanilla && git pull"
+ssh webuser@midnight-worker.de "cd /var/www/html/B7-Website_Vanilla && git pull && git pull origin develop"
