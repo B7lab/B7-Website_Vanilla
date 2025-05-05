38c872ef2af8a2c658efd9ee2b2bcc12fab932df
Decipher3859 <accounts@damianschumachers.com>
2025-05-05 14:49:00 +0200

    .env eingerichtet für Datenbankzugriff.

--- views/area/umbauarbeiten.php
+++ views/area/umbauarbeiten.php
@@ -9,5 +9,11 @@
         <p>
             Content
         </p>
+
+        <div class="image-effect">
+            <img src="/public/img/areas/umbauarbeiten/Dacharbeiten.jpg" alt="Bildbeschreibung" class="color-img">
+            <img src="/public/img/areas/umbauarbeiten/Dacharbeiten.jpg" alt="Bildbeschreibung" class="bw-img">
+            <div class="grain"></div>
+        </div>
     </div>
 </main
