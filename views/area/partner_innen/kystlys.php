9603c1e7e532f7f7869ae21692c9b456097cdef0
Decipher3859 <accounts@damianschumachers.com>
2025-05-06 08:58:38 +0200

    Testphase: Design für Partnerseiten.

--- views/area/partner_innen/kystlys.php
+++ views/area/partner_innen/kystlys.php
@@ -7,6 +7,26 @@
     ?>
     <div class="content-container">
         <h2 class="block-heading">Hipster ipsum</h2>
+        <div class="info-box float-right">
+            <table class="info-table">
+                <tr>
+                    <th>Partner seit</th>
+                    <td>2022</td>
+                </tr>
+                <tr>
+                    <th>TEXT</th>
+                    <td>text</td>
+                </tr>
+                <tr>
+                    <th>TEXT</th>
+                    <td>text</td>
+                </tr>
+                <tr>
+                    <th>Kontakt</th>
+                    <td>kystlys.eu</td>
+                </tr>
+            </table>
+        </div>
         <p>
         I'm baby food truck poutine freegan, organic thundercats normcore brunch hell of blackbird spyplane kombucha. 
         VHS chillwave Brooklyn yuccie mustache trust fund readymade keytar 90's praxis ethical intelligentsia sriracha. 
@@ -15,7 +35,43 @@
         ethical fanny pack. Hoodie tattooed mustache typewriter lumbersexual vibecession coloring book roof party grailed kinfolk 
         blackbird spyplane meditation before they sold out marfa viral. Marxism listicle retro, selfies forage ramps quinoa 
         hoodie irony blackbird spyplane pork belly ascot.
+
+        I'm baby food truck poutine freegan, organic thundercats normcore brunch hell of blackbird spyplane kombucha. 
+        VHS chillwave Brooklyn yuccie mustache trust fund readymade keytar 90's praxis ethical intelligentsia sriracha. 
+        8-bit fanny pack iceland cardigan. Activated charcoal kombucha pickled synth, hella tofu chillwave squid blog waistcoat 
+        air plant plaid skateboard. Keytar DIY XOXO, art party ugh unicorn readymade freegan hella DSA taxidermy la croix man bun 
+        ethical fanny pack. Hoodie tattooed mustache typewriter lumbersexual vibecession coloring book roof party grailed kinfolk 
+        blackbird spyplane meditation before they sold out marfa viral. Marxism listicle retro, selfies forage ramps quinoa 
+        hoodie irony blackbird spyplane pork belly ascot.
+
+        I'm baby food truck poutine freegan, organic thundercats normcore brunch hell of blackbird spyplane kombucha. 
+        VHS chillwave Brooklyn yuccie mustache trust fund readymade keytar 90's praxis ethical intelligentsia sriracha. 
+        8-bit fanny pack iceland cardigan. Activated charcoal kombucha pickled synth, hella tofu chillwave squid blog waistcoat 
+        air plant plaid skateboard. Keytar DIY XOXO, art party ugh unicorn readymade freegan hella DSA taxidermy la croix man bun 
+        ethical fanny pack. Hoodie tattooed mustache typewriter lumbersexual vibecession coloring book roof party grailed kinfolk 
+        blackbird spyplane meditation before they sold out marfa viral. Marxism listicle retro, selfies forage ramps quinoa 
+        hoodie irony blackbird spyplane pork belly ascot.
         </p>
+        <div class="info-box float-right">
+            <table class="info-table">
+                <tr>
+                    <th>Partner seit</th>
+                    <td>2022</td>
+                </tr>
+                <tr>
+                    <th>TEXT</th>
+                    <td>text</td>
+                </tr>
+                <tr>
+                    <th>TEXT</th>
+                    <td>text</td>
+                </tr>
+                <tr>
+                    <th>Kontakt</th>
+                    <td>kystlys.eu</td>
+                </tr>
+            </table>
+        </div>
     </div>  
 </main>
 
